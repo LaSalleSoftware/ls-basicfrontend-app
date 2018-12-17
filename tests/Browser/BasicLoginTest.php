7179d756351f44df20c796f9da037f0e69018afa
Bob Bloom <bob.bloom@lasallesoftware.ca>
2018-12-17 14:32:42 -0500

    Finish basic auth based on artisan auth:make. #1

---
+++ tests/Browser/BasicLoginTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\User;
+
+class ExampleTest extends DuskTestCase
+{
+
+    use DatabaseMigrations;
+
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Laravel');
+        });
+    }
+
+    public function testBasicLogin()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'krugerbloom@gmail.com',
+        ]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visit('/login')
+                    ->type('email', $user->email)
+                    ->type('password', 'secret')
+                    ->press('Login')
+                    ->pause(2000)
+                    ->assertPathIs('/home');
+        });
+    }
+}
