13d550daaadeb24fa2dc801fec458796fa3d058f
Bob Bloom <bob.bloom@lasallesoftware.ca>
2019-07-11 21:31:20 -0400

    Changes related to tagging version 1.9.2. #7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 # CHANGE LOG
 
-## VERSION 2.0 (23/11/2018)
+## VERSION 2.0 (coming!)
 
-### All GitHub issues for this version
-* [https://github.com/lasallesoftware/lsv2-basicfrontend-app/issues?q=milestone%3A%22Version+2.0%22+is%3Aclosed](https://github.com/lasallesoftware/lsv2-basicfrontend-app/issues?q=milestone%3A%22Version+1.0%22+is%3Aclosed)
+
+## VERSION 1.9 PRE-RELEASE 2.0
+
+### All GitHub issues for version 1.9.2 (July 11, 2019)
+* [v1.9.2 issues](https://github.com/LaSalleSoftware/lsv2-basicfrontend-app/milestone/1?closed=1)
---
+++ app/Version.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * This file is part of the LaSalle Software Basic Front-end application.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright  (c) 2019 The South LaSalle Trading Corporation
+ * @license    http://opensource.org/licenses/MIT MIT
+ * @author     Bob Bloom
+ * @email      bob.bloom@lasallesoftware.ca
+ * @link       https://lasallesoftware.ca
+ * @link       https://packagist.org/packages/lasallesoftware/lsv2-basicfrontend-app
+ * @link       https://github.com/LaSalleSoftware/lsv2-basicfrontend-app
+ *
+ */
+namespace App;
+
+class Version
+{
+    /**
+     * This package's version number.
+     *
+     * @var string
+     */
+    const VERSION = '1.9.2';
+
+    /**
+     * This package's name.
+     *
+     * @var string
+     */
+    const PACKAGE = 'Basic Front-end application for my LaSalle Software Version 2';
+
+    /**
+     * Get the version number of this package.
+     *
+     * @return string
+     */
+    public function version()
+    {
+        return static::VERSION;
+    }
+
+    /**
+     * Get the name of this package.
+     *
+     * @return string
+     */
+    public function packageName()
+    {
+        return static::PACKAGE;
+    }
+}
